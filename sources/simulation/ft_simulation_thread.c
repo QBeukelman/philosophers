c65b2b253627f7b45ed576f5e0305ffca8059321
QBeukelman <98453355+QBeukelman@users.noreply.github.com>
2023-03-23 15:30:01 +0100

    updated sim thread (observer)

--- sources/simulation/ft_simulation_thread.c
+++ sources/simulation/ft_simulation_thread.c
@@ -6,7 +6,7 @@
 /*   By: qbeukelm <qbeukelm@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/21 10:27:31 by qbeukelm      #+#    #+#                 */
-/*   Updated: 2023/03/23 14:17:06 by quentinbeuk   ########   odam.nl         */
+/*   Updated: 2023/03/23 15:28:03 by quentinbeuk   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,8 @@ void		*ft_simulation_thread(void *arg)
 
 	while (1)
 	{
+		if (ft_check_die(self) != SUCCESS)
+			break ;
 		ft_eating(self);
 		ft_sleep(self);
 		ft_think(self);
@@ -40,4 +42,47 @@ void		*ft_simulation_thread(void *arg)
 	return (NULL);
 }
 
+int		ft_check_die(t_philo *self)
+{
+	pthread_mutex_lock(&self->data->mutex[DIED]);
+	if (self->data->died == TRUE)
+	{
+		return (FALSE);
+	}
+	pthread_mutex_unlock(&self->data->mutex[DIED]);
+	return (SUCCESS);
+}
 
+// ===== [ observe ] =====
+static int		ft_observe_thread(t_philo *self, t_data *data)
+{
+	int				i;
+	unsigned long	l_meal;
+	
+	i = 0;
+	while (1)
+	{
+		pthread_mutex_lock(&data->mutex[MEALS]);
+		l_meal = self[i].last_meal;
+		pthread_mutex_unlock(&data->mutex[MEALS]);
+		if (ft_set_are_done(self, data, l_meal) != SUCCESS)
+			break ;
+		// ft_set_is_dead()
+	}
+
+	return (SUCCESS);
+}
+
+static int	ft_set_are_done(t_philo *self, t_data *data, unsigned long l_meal)
+{
+	if (l_meal && ft_all_done(self, data))
+	{
+		ft_done(data);
+		return (FAILURE);
+	}
+	return (SUCCESS);
+}
+
+// static int	ft_set_is_dead(t_philo *self, t_data *data)
+// static int	ft_all_done(t_philo *self, t_data *data)
+// void		ft_done(t_data *data)
